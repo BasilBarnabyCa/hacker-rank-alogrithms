e655d15593c7e994a902253e04885d349822de4b
Basil Barnaby <basil.barnaby@devmassive.com>
2023-03-14 11:45:15 -0400

    Added Apple and Orange

---
+++ Apple and Orange.cs
@@ -0,0 +1,91 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'countApplesAndOranges' function below.
+     *
+     * The function accepts following parameters:
+     *  1. INTEGER s
+     *  2. INTEGER t
+     *  3. INTEGER a
+     *  4. INTEGER b
+     *  5. INTEGER_ARRAY apples
+     *  6. INTEGER_ARRAY oranges
+     */
+
+    public static void countApplesAndOranges(int s, int t, int a, int b, List<int> apples, List<int> oranges)
+    {
+        // Expanding variables for clarity
+        int roofLeftSide = s;  // first input line
+        int roofRightSide = t; // first input line
+        int appleTreeLoc = a;  // second input line
+        int orangeTreeLoc = b; // second input line
+        List<int> appleDistances = apples; // fourth input line
+        List<int> orangeDistances = oranges; // fifth input line
+        int applesOnRoof = 0;
+        int orangesOnRoof = 0;
+        
+        foreach(int appleDistance in appleDistances) {
+            int appleLoc = appleTreeLoc + appleDistance;
+            if(appleLoc >= roofLeftSide && appleLoc <= roofRightSide) {
+                applesOnRoof = applesOnRoof + 1;
+            }
+        }
+        
+        foreach(int orangeDistance in orangeDistances) {
+            int orangeLoc = orangeTreeLoc + orangeDistance;
+            if(orangeLoc >= roofLeftSide && orangeLoc <= roofRightSide) {
+                orangesOnRoof = orangesOnRoof + 1;
+            }
+        }
+        
+        // Output
+        Console.WriteLine(applesOnRoof);
+        Console.WriteLine(orangesOnRoof);
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int s = Convert.ToInt32(firstMultipleInput[0]);
+
+        int t = Convert.ToInt32(firstMultipleInput[1]);
+
+        string[] secondMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int a = Convert.ToInt32(secondMultipleInput[0]);
+
+        int b = Convert.ToInt32(secondMultipleInput[1]);
+
+        string[] thirdMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int m = Convert.ToInt32(thirdMultipleInput[0]);
+
+        int n = Convert.ToInt32(thirdMultipleInput[1]);
+
+        List<int> apples = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(applesTemp => Convert.ToInt32(applesTemp)).ToList();
+
+        List<int> oranges = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(orangesTemp => Convert.ToInt32(orangesTemp)).ToList();
+
+        Result.countApplesAndOranges(s, t, a, b, apples, oranges);
+    }
+}
